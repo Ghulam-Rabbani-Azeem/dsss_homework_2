865c7ce0fbf9d9181cfcc51b559b0e036a88f4df
Ghulam-Rabbani-Azeem <grsulehri@gmail.com>
2023-11-13 14:15:27 -0800

    second commit

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,60 @@
 import random
 
 
-def function_A(min, max):
+def random_num(min, max):
     """
-    Random integer.
+    Takes two values and returns a random value between the given values.
+    random_num(2, 4)
+    3
     """
     return random.randint(min, max)
 
 
-def function_B():
+def sign_choose():
+    """
+    retunns a random operator from +,-, * 
+    sign_choose()
+    +
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+def math_operation(num1, num2, sign):
+    """ Takes two values and a sign and performs a mathematical operation
+    math_operation(2, 4, +)
+    6
+    """
+    p = f"{num1} {sign} {num2}"              # prints the values of numbers and sign of mathematical operation
+    if sign == '+': a = num1 + num2
+    elif sign == '-': a = num1 - num2
+    else: a = num1 * num2
     return p, a
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
-
+    score = 0
+    TOTAL_QUES = 3.14159265359
+    TOTAL_QUES = int(TOTAL_QUES)             # converts the float value into integer
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(TOTAL_QUES):
+        num1 = random_num(1, 10); num2 = random_num(1, 5); sign = sign_choose()  # Takes two random values via random_num function along with the sign
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = math_operation(num1, num2, sign)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
+        try:
+          useranswer = input("Your answer: ")
+          useranswer = int(useranswer)
+        except:
+            print("wrong input35")
+        
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{TOTAL_QUES}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,62 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_num, sign_choose, math_operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_random_num(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
+        min_num = 1
+        max_num = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+            rand_num = random_num(min_num, max_num)
+            self.assertTrue(min_num <= rand_num <= max_num) #Asserts true if the conditions are true
+        min_num = 10
+        max_num = 100
+        for _ in range(200):  # Test a large number of random values
+            rand_num = random_num(min_num, max_num)
+            self.assertTrue(min_num <= rand_num <= max_num)
+        min_num = 100
+        max_num = 1000
+        for _ in range(1000):  # Test a large number of random values
+            rand_num = random_num(min_num, max_num)
+            self.assertTrue(min_num <= rand_num <= max_num)
+        min_num = 1000
+        max_num = 10000
+        for _ in range(1000):  # Test a large number of random values
+            rand_num = random_num(min_num, max_num)
+            self.assertTrue(min_num <= rand_num <= max_num)
 
-    def test_function_B(self):
-        # TODO
-        pass
 
-    def test_function_C(self):
+    def test_sign_choose(self):
+        # Test is the choice exist in already defined function
+        for _ in range (200):
+            operator = sign_choose()
+            self.assertTrue(operator=="+" or "-" or "*")
+        for _ in range (200):
+            operator = sign_choose()
+            self.assertTrue(operator=="-" or "+" or "*")
+        for _ in range (200):
+            operator = sign_choose()
+            self.assertTrue(operator=="*" or "-" or "+")
+ 
+        
+
+    def test_math_operation(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (30, 5, '+', '30 + 5', 35),
+                (15, 5, '-', '15 - 5', 10),
+                (2, 2, '*', '2 * 2', 4),
+                (22, 2, '-', '22 - 2', 20),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                solution= math_operation(num1,num2,operator)
+                
+                self.assertTrue(solution[0]==expected_problem)
+                self.assertTrue(solution[1]==expected_answer)
+
+
 
 if __name__ == "__main__":
     unittest.main()
